80b22d8b156c9866d3f2c660008d53c0f9085c45
Ivan7111 <ivan.hrytsel.jazzteam@gmail.com>
2022-09-13 14:55:17 +0300

    Fixed formatting

--- Task1.php
+++ Task1.php
@@ -4,7 +4,6 @@ namespace src;
 
 class Task1
 {
-
     public static function main(int $inputNumber): string
     {
         return Task1::moreThanWhat($inputNumber);
@@ -15,5 +14,4 @@ class Task1
         return $number > $compareTo ? 'More than ' . $compareTo :
             ($compareTo > 10 ? Task1::moreThanWhat($number, $compareTo - 10) : 'Equal or less than 10');
     }
-
 }

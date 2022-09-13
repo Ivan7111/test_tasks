27dab1864f882e40ba032babc0edfc52802a3ba8
Ivan7111 <ivan.hrytsel.jazzteam@gmail.com>
2022-09-13 14:07:23 +0300

    Task 1 first draft

---
+++ Task1.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace src;
+
+class Task1
+{
+
+    public static function main(int $inputNumber): string
+    {
+        return Task1::moreThanWhat($inputNumber);
+    }
+
+    private static function moreThanWhat(int $number, int $compareTo = 30): string
+    {
+        return $number > $compareTo ? 'More than ' . $compareTo :
+            ($compareTo > 10 ? Task1::moreThanWhat($number, $compareTo - 10) : 'Equal or less than 10');
+    }
+
+}
